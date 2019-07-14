702f4f65ea3db9b1fff133fe0f515ab1e8d75741
Kentaro Fujiy <kentaro@manacadigital.com.br>
2019-07-14 04:15:58 +0000

    supergit

--- lib/generators/bootstrap/themed/themed_generator.rb
+++ lib/generators/bootstrap/themed/themed_generator.rb
@@ -61,7 +61,7 @@ module Bootstrap
       end
 
       def excluded_columns_names
-        %w[_id _type id created_at updated_at]
+        %w[_id _type id avatar created_at updated_at]
       end
 
       def excluded_columns_pattern
